681c7ed9ddc65e499409ec89995924564b18035d
Mikinyil <raicu.bogdan@cnmv.ro>
2025-07-21 16:32:32 +0300

    Mama mia Mehe Pizza

--- IntroToPy/Randoms_1.py
+++ IntroToPy/Randoms_1.py
@@ -18,7 +18,7 @@ for i in range(0, count):
     randoms_map[r].append(i)
 
 # Write a text file "randoms_db.txt" with each random on a line, its value followed by the indexes where it occurred
-with open("IntroToPy/randoms_shaorma.txt", "w") as data_file:
+with open("IntroToPy/pizza.txt", "w") as data_file:
     for r in randoms_map.keys():
         data_file.write(f"{r} {randoms_map[r]}\n")
 
